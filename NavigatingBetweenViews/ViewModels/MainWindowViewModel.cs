10fd4a6786b4904feaba53a554c1a48ea970fa50
med <mohamuza@gmail.com>
2025-07-06 21:38:49 +0100

    added core files

---
+++ NavigatingBetweenViews/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace NavigatingBetweenViews.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty] private ViewModelBase _currentViewModel;
+
+    public MainWindowViewModel()
+    {
+        CurrentViewModel = new HomeViewModel();
+    }
+
+    [RelayCommand]
+    private void NavigateToHome()
+    {
+        CurrentViewModel = new HomeViewModel();
+    }
+
+    [RelayCommand]
+    private void NavigateToSettings()
+    {
+        CurrentViewModel = new SettingsViewModel();
+    }
+
+    [RelayCommand]
+    private void NavigateToAbout()
+    {
+        CurrentViewModel = new AboutViewModel();
+    }
+}
